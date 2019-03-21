f6b0f399e5c281b0f91dfcad84f9f1cb60404792
howon <howone1sky@gmail.com>
2019-03-21 17:55:04 +0300

    корекция

--- app/views.py
+++ app/views.py
@@ -8,11 +8,11 @@ from app import app
 def login():
     form = forms.LoginForm()
     if form.validate_on_submit():
-        flash('login ' + form.login.data + 'password ' + form.password.data + ', remember_me = ' + str(form.remember_me.data))
+        flash('login ' + form.login.data + ',  password ' + form.password.data + ', remember_me = ' + str(form.remember_me.data))
         if form.men.data:
-            flash(str(form.men.data))
+            flash('men ' + str(form.men.data))
         if form.women.data:
-            flash(str(form.women.data))
+            flash('women ' + str(form.women.data))
 
         return redirect('/index')
     return render_template('login.html', form=form)
